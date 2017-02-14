7fb0f53a64e85e41103f32ab7e69257118aed2da
beingMayuresh <mayureshgupta@hotmail.com>
2017-02-14 00:23:32 -0500

    1st
    commit

---
+++ src/main/java/org/mayuresh/gupta/messenger/database/DatabaseClass.java
@@ -0,0 +1,26 @@
+package org.mayuresh.gupta.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.mayuresh.gupta.messenger.model.Message;
+import org.mayuresh.gupta.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<String, Profile> getProfiles() {
+		return profiles;
+	}
+
+	
+	
+	
+}
